c78c982fa9d4c10ac22573bae12bedb27e8723a0
Ian Moses <ian.moses@cognizant.com>
2023-05-03 11:56:35 -0700

    added framerate independence

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.03f;
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 10f;
 
     void Start()
     {
@@ -14,10 +14,10 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
 
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(0, moveAmount, 0);
     }
 }
